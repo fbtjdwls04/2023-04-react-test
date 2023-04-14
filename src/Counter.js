58d160db2dece35ff84136cb524f221da9049b65
fbtjdwls04 <fbtjdwls04@gmail.com>
2023-04-14 21:43:18 +0900

    입력 제어 및 카운팅

--- src/Counter.js
+++ src/Counter.js
@@ -4,34 +4,28 @@ export default function Counter(){
 
     const [no, setNo] = useState(0);
     const noIsEvenDiv = (
-        no % 2 == 0 ? <>
+    <>
         <hr/>
-        <span>짝수입니다</span>
-        </>
-        :
-        <>
-            <hr/>
-            <span>홀수입니다</span>
-        </>
-    )
-    const eightDev = (
-        no % 8 == 0 ?
-        <>
-            <hr/>
-            <div>8의 배수입니다.</div>
-        </>
-        :
-        <>
-        </>
-        )
+        {no%2 == 0 ? <div>짝수입니다.</div> : <div>홀수입니다</div>}
+    </>
+    );
 
+    const noIsNot8NultipleDiv = no%8 == 0 || (
+        <>  
+            <hr/>
+            <div>
+                8의 배수가 아닙니다.
+            </div>
+        </>
+    );
     return (
     <>
         숫자 : {no}
         <hr/>
         <button onClick={()=> setNo(no+1)}>중가</button>
         {noIsEvenDiv}
-        {eightDev}
+        {noIsNot8NultipleDiv}
+        
     </>
-    )
+    );
 }
