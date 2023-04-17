15ca8f258ff678b2f8db4fd39a3f1e6a352f5bcb
fbtjdwls04 <fbtjdwls04@gmail.com>
2023-04-17 19:58:27 +0900

    팝업기능 및 버튼, 데이지 UI 및 테일윈드 css 적용

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React,{useState} from "react";
 import Control from "./Control";
 import SetTimeOut from "./SetTimeOut";
 import Counter from "./Counter";
+import Popup from "./Popup";
 function App() {
 
   return (
     <>
       {/* <Control></Control> */}
       {/* <SetTimeOut></SetTimeOut> */}
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <Popup></Popup>
     </>
   );
 }
