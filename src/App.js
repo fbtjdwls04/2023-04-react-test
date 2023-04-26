caf5358e3c40e0413d7a65d3c85a381d6c4f3e81
fbtjdwls04 <fbtjdwls04@gmail.com>
2023-04-26 21:18:38 +0900

    다크모드 구현

--- src/App.js
+++ src/App.js
@@ -1,66 +1,41 @@
 import React,{useEffect, useState, useRef} from "react";
 
 let AppCallCount = 0;
-let SubCallcount = 0;
-
-function Sub({appNo}) {
-  SubCallcount++;
-  console.log(`SubCallcount : ${SubCallcount}`);
-  const [no, SetNo] = useState(0);
-  const [no2, SetNo2] = useState(0);
-
-  useEffect(()=>{
-    console.log('effect 1 : 단 한번 실행');
-  },[])
-
-  useEffect(()=>{
-    console.log('effect 2 : 부모(App)의 appNo가 바뀔 때마다 실행');
-  },[appNo])
-
-  useEffect(()=>{
-    console.log('effect 3 : 나(Sub)가 바뀔 때마다 실행');
-  },[no])
-  useEffect(()=>{
-    console.log('effect 4 : appNo 혹은 no가 바뀔 때마다 실행');
-  },[appNo, no])
-  useEffect(()=>{
-    console.log('effect 5 : 매번 실행');
-  })
-
-
-  return (
-    <>
-      <div style={{
-        border : "10px solid blue",
-        padding : 10
-      }}>
-        App no : {appNo}
-        <button className="btn btn-outline" onClick={()=>{SetNo(no + 1 )}}>
-          Sub 버튼 : {no}
-        </button>
-        <button className="btn btn-outline" onClick={()=>{SetNo2(no2 + 1 )}}>
-          App 버튼 : {no2}
-        </button>
-      </div>
-    </> 
-  );
-}
 
 function App() {
   AppCallCount++;
   console.log(`AppCallCount : ${AppCallCount}`);
   const [no, SetNo] = useState(0);
-  
+  const [isDark, SetIsDark] = useState(false);
+
+  useEffect(()=>{
+    const html = document.getElementsByTagName('html')[0];
+    if(isDark){
+      html.classList.add('dark');
+    } else {
+      html.classList.remove('dark');
+    }
+  },[isDark]);
+
   return (
     <>
       <div style={{
-        border : "10px solid red",
         padding : 10
       }}>
         <button className="btn btn-outline" onClick={()=>{SetNo(no + 1 )}}>
           App 버튼 : {no}
         </button>
-        <Sub appNo={no}/>
+        <button className="btn btn-outline" onClick={()=>{SetIsDark(!isDark)}}>
+          테마 토글
+        </button>
+
+        <hr />
+
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat possimus porro, eveniet odio exercitationem harum, dolorum molestias aliquid sed quas consequatur, nostrum nam omnis et quos pariatur! Reprehenderit, illo ducimus.
+        </div>
+
+        <h1 class="color-primary">안녕, 반가워</h1>
       </div>
     </> 
   );
